2bc6ee2ec0c39f8fbb5daa752a6ac888d782e5b5
Pedro César (Alface) <pedrocesar1410@gmail.com>
2018-09-01 11:24:39 -0300

    Update jogoDaVelha.js

--- jogoDaVelha/jogoDaVelha.js
+++ jogoDaVelha/jogoDaVelha.js
@@ -3,16 +3,18 @@ for(let i = 0; i <= 8; i++)
 	tabuleiro[i] = 0;
 
 let jogadores = [{ tipo: 'n', simbolo: 'white'},
-				 { tipo: 'j', simbolo: 'blue' },
-		   	     { tipo: 'j', simbolo: 'red'  }];
+				 { tipo: 'c', simbolo: 'blue' },
+		   	     { tipo: 'c', simbolo: 'red'  }];
 let vez = 1;
 let casasPreenchidas = 0;
+if(jogadores[1].tipo != 'j') jogadaComputador(1);
 
 function reinicia() {
 	vez = 1;
 	casasPreenchidas = 0;
 	for(let i = 0; i <= 8; i++)
 		preencheCasa(i, 0)
+	if(jogadores[1].tipo != 'j') jogadaComputador(1);
 }
 
 function preencheCasa(i, jogador) {
@@ -40,6 +42,22 @@ function analisaVencedor() {
 	return casasPreenchidas == 9 ? 3 : 0;
 }
 
+function alteraVez() {
+	let analise = analisaVencedor();
+	if(analise == 0) {
+		vez = (vez == 1) ? 2 : 1;
+		if(jogadores[vez].tipo != 'j') 
+			jogadaComputador(vez);
+	}
+	else if(analise == 1 || analise == 2) {
+		console.log('jogador ' + vez + ' venceu');
+		vez = 0;
+	} else {
+		console.log('deu velha');
+		vez = 0;
+	}
+}
+
 $('#botao-reiniciar').click(reinicia);
 
 function analisaPossibilidadeVitoria(i1, i2, i3, jogador) {
@@ -49,38 +67,48 @@ function analisaPossibilidadeVitoria(i1, i2, i3, jogador) {
 		return i2;
 	if(tabuleiro[i1] == jogador && tabuleiro[i2] == jogador && tabuleiro[i3] == 0)
 		return i3;
+	return -1;
 }
 
-function analisaCasoVitoriaCerta(jogador) {
+function casoVitoria(jogador) {
 	let casa = -1;
 	for(let i = 0; i <= 2; i++) {
 		if((casa = analisaPossibilidadeVitoria(i, i + 3, i + 6, jogador)) != -1)
 			return casa;
 		if((casa = analisaPossibilidadeVitoria(3 * i, 3 * i + 1, 3 * i + 2, jogador)) != -1)
 			return casa;
-		if((casa = analisaPossibilidadeVitoria(4 - 2 * i, 4, 4 + 2 * i, jogador)) != -1)
+		if(i && (casa = analisaPossibilidadeVitoria(4 - 2 * i, 4, 4 + 2 * i, jogador)) != -1)
 			return casa;
 	}
 	return -1;
 }
 
+function casoDerrota(jogador) {
+	return casoVitoria(jogador == 1 ? 2 : 1);
+}
+
+function casoAleatorio() {
+	let v = [];
+	for(let i = 0; i <= 8; i++) {
+		if(tabuleiro[i] == 0) v.push(i);
+	}
+	return v[Math.floor(Math.random() * v.length)];
+}
+
 function jogadaComputador(jogador) {
-	let casa = -1;
-	if((casa = analisaCasoVitoriaCerta(jogador)) != -1) preencheCasa(casa, jogador);
+	setTimeout(function() {
+		let casa;
+		if((casa = casoVitoria(jogador)) != -1) preencheCasa(casa, jogador);
+		else if((casa = casoDerrota(jogador)) != -1) preencheCasa(casa, jogador);
+		else preencheCasa(casoAleatorio(), jogador);
+		alteraVez();
+	}, 500);
 }
 
 $('.casa').click(function() {
 	let indice = $('.casa').index($(this));
 	if(vez && jogadores[vez].tipo == 'j' && tabuleiro[indice] == 0) {
 		preencheCasa(indice, vez);
-		let vencedor = analisaVencedor();
-		if(vencedor) {
-			console.log(vencedor);
-			vez = 0;
-		} else {
-			vez = (vez == 1) ? 2 : 1;
-			if(jogadores[vez].tipo != 'j') 
-				jogadaComputador(vez);
-		}
+		alteraVez();
 	}
 });
