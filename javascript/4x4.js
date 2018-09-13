53da688528866f484c258f02c90ff5001ca32669
Pedro César (Alface) <pedrocesar1410@gmail.com>
2018-09-12 21:09:26 -0300

    Delete 4x4.js

--- javascript/4x4.js
+++
@@ -1,9 +0,0 @@
-let ttt = new TicTacToe(4, 4, 3, 2, $('.casa'), $('#log'));
-
-$('.casa').click(function() {
-	ttt.jogadaUsuario($('.casa').index($(this)));
-});
-
-$('#botao-reiniciar').click(function() {
-	ttt.reinicia();
-});
