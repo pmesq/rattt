4b08b1c7b3e5c0aa5c847438a8a317193dc8cfa4
Pedro César (Alface) <pedrocesar1410@gmail.com>
2018-10-15 17:15:27 -0300

    Add files via upload

--- javascript/menu.js
+++ javascript/menu.js
@@ -16,3 +16,41 @@ $menu.mouseout(function() {
         $menu.css('left', '-184px');
     $menu.mousemove(function() { $menu.css('left', '0'); });
 });
+
+let $botaoCampanha = $('#a-campanha');
+let $setaCampanha = $('#a-campanha > span');
+let $campanha = $('#campanha');
+let campanhaAberta = false;
+
+$campanha.slideUp();
+$campanha.css('display', 'auto');
+
+$botaoCampanha.click(function() {
+	campanhaAberta = !campanhaAberta;
+	if(campanhaAberta) {
+		$setaCampanha.html('▲');
+		$campanha.slideDown();
+	} else {
+		$setaCampanha.html('▼');
+		$campanha.slideUp();
+	}
+});
+
+let $botaoJogar = $('#a-jogar');
+let $setaJogar = $('#a-jogar > span');
+let $jogar = $('#jogar');
+let jogarAberto = false;
+
+$jogar.slideUp();
+$jogar.css('display', 'auto');
+
+$botaoJogar.click(function() {
+	jogarAberto = !jogarAberto;
+	if(jogarAberto) {
+		$setaJogar.html('▲');
+		$jogar.slideDown();
+	} else {
+		$setaJogar.html('▼');
+		$jogar.slideUp();
+	}
+});
