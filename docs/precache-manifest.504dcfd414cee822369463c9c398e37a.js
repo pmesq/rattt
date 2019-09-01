8c0aa550f6c86e8bf898aadb36077347fc47b1e0
PedroCesarMesquita <pedrocesar1410@gmail.com>
2019-09-01 13:43:00 -0300

    Docs

---
+++ docs/precache-manifest.504dcfd414cee822369463c9c398e37a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9606b9436b05c2d1239d8261216d532b",
+    "url": "/rattt/index.html"
+  },
+  {
+    "revision": "c53aa8844becba02fe8e",
+    "url": "/rattt/static/css/main.007a0c74.chunk.css"
+  },
+  {
+    "revision": "93cfd06d0e93476d040b",
+    "url": "/rattt/static/js/2.5ca244a9.chunk.js"
+  },
+  {
+    "revision": "c53aa8844becba02fe8e",
+    "url": "/rattt/static/js/main.24380743.chunk.js"
+  },
+  {
+    "revision": "43c99d1c2ee635f03f58",
+    "url": "/rattt/static/js/runtime~main.100848bc.js"
+  }
+]);
